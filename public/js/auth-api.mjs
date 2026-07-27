ef571ccf3cdca2716b2fb2854bcd4cdfdfbeb2a8
manontheluna <eluna-ortez@csumb.edu>
2026-07-27 16:52:16 -0700

    feat: register user in db

--- public/js/auth-api.mjs
+++ public/js/auth-api.mjs
@@ -1,11 +1,22 @@
 import bcrypt from 'bcrypt'
+import { db } from '../../db/db.mjs'
 
 export async function registerUser(req, res) {
-    const { firstName, lastName, email, password } = req.body
-    console.log(firstName, lastName, email)
-    // salt rounds for bcrypt
-    const rounds = 10
-    const hashedPassword = await bcrypt.hash(password, rounds)
-
-    res.redirect('/login')
+    try {
+        const { firstName, lastName, email, password } = req.body
+        console.log(firstName, lastName, email)
+        // salt rounds for bcrypt
+        const rounds = 10
+        const hashedPassword = await bcrypt.hash(password, rounds)
+        // insert record into database
+        const query = `
+            INSERT INTO fp_users(firstName, lastName, email, password_hash, isAdmin) VALUES (?, ?, ?, ?, ?)
+        `
+        const values = [firstName, lastName, email, hashedPassword, 0]
+        const [result] = await db.execute(query, values)
+        res.redirect('/login')
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('Registration Failed')
+    }
 }
