38303a37f7510e8935778494dca206084e85872d
manontheluna <eluna-ortez@csumb.edu>
2026-07-31 15:22:23 -0700

    refactor: add event listener and form pre-fill for all pantry items

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,17 @@
+// general logic to add even listener to all edit buttons of ingredient
+// elements. TODO: refactor this out to its own module for scalability,
+// dependent on behavior or editing user flow
+
 const editPantry = document.querySelector('.edit-pantry')
 
-// ingredient/pantry form
-editPantry?.addEventListener('click', () => {
-    console.log(editPantry.dataset.ingredient)
-    const ingredient = JSON.parse(editPantry.dataset.ingredient)
-    console.log('ingredient: ', ingredient)
-    document.querySelector('[name="ingredientName"]').value = ingredient.name ?? ''
-    document.querySelector('[name="description"]').value = ingredient.description ?? ''
-    document.querySelector('[name="quantity"]').value = ingredient.quantity ?? ''
-    document.querySelector('[name="unit"]').value = ingredient.unit ?? ''
-    document.querySelector('[name="expiration"]').value = ingredient.expirationDate?.split('T')[0] || ''
+document.querySelectorAll('.edit-pantry').forEach(button => {
+    button.addEventListener('click', () => {
+        const ingredient = JSON.parse(button.dataset.ingredient)
+        console.log('ingredient: ', ingredient)
+        document.querySelector('[name="ingredientName"]').value = ingredient.name ?? ''
+        document.querySelector('[name="description"]').value = ingredient.description ?? ''
+        document.querySelector('[name="quantity"]').value = ingredient.quantity ?? ''
+        document.querySelector('[name="unit"]').value = ingredient.unit ?? ''
+        document.querySelector('[name="expiration"]').value = ingredient.expirationDate?.split('T')[0] || ''
+    })
 })
